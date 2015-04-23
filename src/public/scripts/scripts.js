5be50287cc86717f37e9ff98f69cdce9ed36fe0b
Jon Ambas <ambasp@gmail.com>
2015-04-22 21:44:31 -0400

    update much

---
+++ src/public/scripts/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	$(window).scroll($.throttle( 150, function(){
+		slideIn();
+	}));
+});
+
+$(window).load(slideIn);
+$(window).resize(slideIn);
+
+function slideIn() {
+	$('.js-slide-in').each(function(i, el){
+		if (isElementInViewport(el)) $(el).addClass('is-visible');
+	});
+}
+
+function isElementInViewport (el) {
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+    var rect = el.getBoundingClientRect();
+ 
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
